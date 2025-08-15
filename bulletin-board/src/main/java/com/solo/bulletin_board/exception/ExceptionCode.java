a8f231e3d6e6d8a6385dd056917bda75960b0a87
0KSY <uryhon7@gmail.com>
2025-08-15 16:11:40 +0900

    개발 환경 세팅 및 member/posting 기본 동작 구현

---
+++ bulletin-board/src/main/java/com/solo/bulletin_board/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package com.solo.bulletin_board.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "Member not found"),
+    MEMBER_EXISTS(409, "Member exists"),
+    MEMBER_NICKNAME_EXISTS(409, "Member nickname exists"),
+    POSTING_NOT_FOUND(404, "Posting not found");
+
+    @Getter
+    private int status;
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
