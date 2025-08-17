ecbbc95107a639f203123b86e03004ebe6391ab0
0KSY <uryhon7@gmail.com>
2025-08-17 16:25:19 +0900

    spring security 구현

--- bulletin-board/src/main/java/com/solo/bulletin_board/member/entity/Member.java
+++ bulletin-board/src/main/java/com/solo/bulletin_board/member/entity/Member.java
@@ -1,10 +1,15 @@
 package com.solo.bulletin_board.member.entity;
 
+import com.solo.bulletin_board.comment.entity.Comment;
+import com.solo.bulletin_board.posting.entity.Posting;
+import com.solo.bulletin_board.postingLike.entity.PostingLike;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -19,7 +24,21 @@ public class Member {
     @Column(nullable = false, unique = true, updatable = false)
     private String email;
 
+    private String password;
+
     @Column(nullable = false, unique = true)
     private String nickname;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Posting> postings = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<Comment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.REMOVE)
+    private List<PostingLike> postingLikes = new ArrayList<>();
 
 }
